c99646533db993c0ce806b0552c68bbf0f38c1bd
Adnan Ansari <Adnan@123>
2023-08-27 21:35:43 +0530

    change in userSchema

--- models/userSchema.js
+++ models/userSchema.js
@@ -5,29 +5,30 @@ const validator = require("validator");
 // create users scehma
 
 const userSchema = new mongoose.Schema({
-    Name:{
-        type:String,
-        required:true,
-        trim:true,
+    Name: {
+        type: String,
+        required: true,
+        trim: true,
     },
-  
-    Phone_Number:{
-        type:Number,
-        required:true,
-        unique:true,
-        minlength:10,
-        maxlength:10
+
+    Phone_Number: {
+        type: Number,
+        required: true,
+        unique: true,
+        minlength: 10,
+        maxlength: 10
     },
-    
-    Password:{
-        type:String,
-        required:true,
-        minlength:8
+
+    Password: {
+        type: String,
+        required: true,
+        minlength: 8
     },
-    datecreated:Date,
-    dateUpdated:Date
+    datecreated: Date,
+    dateUpdated: Date
 });
 
 // model define
-const user = new mongoose.model("user",userSchema);
+const user = new mongoose.model("user", userSchema);
 module.exports = user;
+parameter
